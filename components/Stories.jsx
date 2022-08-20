a434c5757cb9f96e0b391500b8a3b12b93d8037d
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-20 07:01:29 +0000

    feat: :sparkles: rendered stories with fakerjs

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { faker } from '@faker-js/faker'
+import { Story } from './components'
 
 const Stories = () => {
+    const [suggestions, setSuggestions] = useState([])
+
+    // Runs when component loads on screen
+    useEffect(() => {
+        // generat 20 arrays with faker-js info
+        const suggest = [...Array(20)].map((_, index) => ({
+            // ...faker.helpers.contextualCard(),
+            id: index,
+            name: faker.name.firstName(),
+            image: faker.image.avatar()
+
+
+        }))
+        setSuggestions(suggest);
+    }, [])
+
   return (
-    <div>Stories</div>
+      <div>
+          {
+              suggestions && suggestions.map((profile) => (
+                  <Story key={profile.id} img={profile.image} username={profile.name} />
+              ))
+          }
+    </div>
   )
 }
 
