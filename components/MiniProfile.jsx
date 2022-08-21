12311c9944bffb7fb4d3f2dcc23a2a852d1f9224
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-21 18:57:44 +0000

    feat: :sparkles: created mini profile section

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const MiniProfile = () => {
-  return (
-    <div>MiniProfile</div>
-  )
+	return (
+		<div className='flex items-center justify-between mt-14 '>
+			<img
+				src='https://htmlstream.com/preview/unify-v2.6/assets/img-temp/400x450/img5.jpg'
+				alt=''
+				className='rounded-full border p-[2px] w-16 h-16 object-cover'
+			/>
+			<div className='flex-1 mx-4'>
+				<h2 className='font-bold'>n.utifafa</h2>
+				<h3 className='text-sm text-gray-500'>Welcome to Instagram</h3>
+			</div>
+			<button className='text-blue-400 font-semibold text-sm'>Sign Out</button>
+		</div>
+	)
 }
 
 export default MiniProfile
