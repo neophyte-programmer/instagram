ca1973d3951e0108f33f6dfe5fff2719ea6a6cdf
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-21 17:02:37 +0000

    feat: :sparkles: created more components

---
+++ components/MiniProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MiniProfile = () => {
+  return (
+    <div>MiniProfile</div>
+  )
+}
+
+export default MiniProfile
---
+++ components/Suggestions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Suggestions = () => {
+  return (
+    <div>Suggestions</div>
+  )
+}
+
+export default Suggestions
