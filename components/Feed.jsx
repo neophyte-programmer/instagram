6e02932fa15380a6618b37c7bb015e6e052daeb7
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-21 18:59:40 +0000

    feat: :sparkles: update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@ import { Stories, Posts, MiniProfile, Suggestions } from './components'
 
 const Feed = () => {
 	return (
-		<main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+		<main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto gap-10'>
 			{/* Main Feed */}
 			<section className='col-span-2'>
 				<Stories />
@@ -12,8 +12,10 @@ const Feed = () => {
 
 			{/* Side Section */}
 			<section className='hidden xl:inline-grid md:col-span-1'>
-        <MiniProfile />
-        <Suggestions />
+				<div className='fixed flex flex-col gap-4'>
+					<MiniProfile />
+					<Suggestions />
+				</div>
 			</section>
 		</main>
 	)
