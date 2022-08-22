ead1edfec1b132721d305c8f7812b36421fcee1a
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 10:49:09 +0000

    feat: :sparkles: firebase stuff

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from 'firebase/app'
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const storage = getStorage()
+
+export { app, db, storage}
