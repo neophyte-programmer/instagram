9e2f2efe02c2dd49441e3f9c39f4e8d2599ef4a8
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 12:54:35 +0000

    feat: :sparkles: update

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -3,7 +3,6 @@ import { signOut, useSession} from "next-auth/react"
 
 const MiniProfile = () => {
 	const { data: session } = useSession()
-	console.log(session)
 
 	return (
 		<div className='flex items-center justify-between mt-14 '>
