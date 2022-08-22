790d13e43750bc3ffcd183e9e93815dba97691b2
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 20:15:11 +0000

    feat: :sparkles: wrapped app in recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot} from "recoil"
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     // Wrap app in session to know if user is authenticated or not
-		<SessionProvider session={session}>
+	  <SessionProvider session={session}>
+		  {/* State Management with recoil */}
+		  <RecoilRoot>
 			<Component {...pageProps} />
+		  </RecoilRoot>
 		</SessionProvider>
 	)
 }
