408a1c6f749480351ecce3cf74f1337c4f0fa166
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 11:51:11 +0000

    feat: :sparkles: dynamic data

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { signOut, useSession} from "next-auth/react"
 
 const MiniProfile = () => {
+	const { data: session } = useSession()
+	console.log(session)
+
 	return (
 		<div className='flex items-center justify-between mt-14 '>
 			<img
-				src='https://htmlstream.com/preview/unify-v2.6/assets/img-temp/400x450/img5.jpg'
+				src={session?.user?.image}
 				alt=''
 				className='rounded-full border p-[2px] w-16 h-16 object-cover'
 			/>
 			<div className='flex-1 mx-4'>
-				<h2 className='font-bold'>n.utifafa</h2>
+				<h2 className='font-bold'>{session?.user?.username}</h2>
 				<h3 className='text-sm text-gray-500'>Welcome to Instagram</h3>
 			</div>
-			<button className='text-blue-400 font-semibold text-sm'>Sign Out</button>
+			<button className='text-blue-400 font-semibold text-sm' onClick={signOut}>Sign Out</button>
 		</div>
 	)
 }
