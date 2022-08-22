4a601b56edcaa6457cf485259cd962cfd8b72c39
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 11:34:39 +0000

    feat: :sparkles: customized signin page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import { getProviders, signIn } from 'next-auth/react'
+import { Header } from '../../components/components'
+import { instagram } from '../../components/images'
 
 //  Access providers from server side by destructuring props
 const SignIn = ({ providers }) => {
@@ -10,26 +13,39 @@ const SignIn = ({ providers }) => {
 				<title>Sign In</title>
 				<link rel='icon' type='image/png' href='/logo.png' />
 			</Head>
-			<>  
-				{Object.values(providers).map((provider) => (
-					<div key={provider.name}>
-						<button onClick={() => signIn(provider.id)}>
-							Sign in with {provider.name}
-						</button>
-					</div>
-				))}
-			</>
+			<Header />
+			<div className='flex flex-col items-center justify-center w-full h-full gap-4 px-6 text-center min-h-[80vh]'>
+				<div className='relative h-64 w-80 '>
+					<Image src={instagram} layout='fill' objectFit='contain' />
+				</div>
+				<p className='font-xs italic text-gray-600 '>
+					This is not the real instagram app. It is just a clone built for self development and educational purposes
+				</p>
+
+				<div>
+					{Object.values(providers).map((provider) => (
+						<div key={provider.name}>
+							<button
+								className='bg-blue-500 p-3 rounded-lg text-white block'
+								onClick={() => signIn(provider.id, {callbackUrl: '/'})}
+							>
+								Sign in with {provider.name}
+							</button>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	)
 }
 
 // Server side rendering
 export async function getServerSideProps(context) {
-    // Gets available providers
-    const providers = await getProviders()
-    // Returns providers to client side
-    return {
-      props: { providers },
-    }
-  }
+	// Gets available providers
+	const providers = await getProviders()
+	// Returns providers to client side
+	return {
+		props: { providers },
+	}
+}
 export default SignIn
