f3bb63e206882dc7da56902d8407475b4c7a40c8
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-22 11:50:43 +0000

    feat: :sparkles: enhanced callback function to return more user info

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,32 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-    ],
-    // theme: {
-    //     logo: "https://www.instagram.com/",
-    //     brandColor: "#BF0194",
-    //     colorScheme: "auto"
-    // }
-    pages: {
-        signIn: "/auth/signin",
-    }
+	// Configure one or more authentication providers
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		// ...add more providers here
+	],
+	// theme: {
+	//     logo: "https://www.instagram.com/",
+	//     brandColor: "#BF0194",
+	//     colorScheme: "auto"
+	// }
+	pages: {
+		signIn: '/auth/signin',
+	},
+	callbacks: {
+		async session({ session, token, user }) {
+			session.user.username = session.user.name
+				.split(' ')
+				.join('')
+				.toLowerCase()
+			session.user.uid = token.sub
+
+			return session
+		},
+	},
 })
